a807854ae7b0c3af5055e12d06884de120d3a610
ianmllr <hrjensen243@gmail.com>
2026-03-10 23:54:41 +0100

    pricerunner now also scrapes for data from telmore_tilgift and callme

--- scrapers/pricerunner_scraper.py
+++ scrapers/pricerunner_scraper.py
@@ -307,12 +307,14 @@ def scrape_pricerunner():
 
     providers = [
         ('data/telmore/telmore_offers.json', 'product_name'),
+        ('data/telmore_tilgift/telmore_tilgift_offers.json', 'product_name'),
         ('data/oister/oister_offers.json', 'product_name'),
         ('data/elgiganten/elgiganten_offers.json', 'product'),
         ('data/cbb/cbb_offers.json', 'product_name'),
         ('data/3/3_offers.json', 'product_name'),
         ('data/yousee/yousee_offers.json', 'product_name'),
         ('data/norlys/norlys_offers.json', 'product_name'),
+        ('data/callme/callme_offers.json', 'product_name'),
     ]
 
     # collect unique product names from all provider files
